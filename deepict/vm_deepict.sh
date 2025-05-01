031c2fa3adc617a8aa5285edb1dc0603b47f2322
Dmitry Molokhov <molokhovdmitry@gmail.com>
2025-05-01 13:53:15 +0300

    Add deepict vm script

---
+++ deepict/vm_deepict.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+mkdir czii-cryo-et-object-identification
+
+git clone https://github.com/ZauggGroup/DeePiCt.git
+git clone https://github.com/molokhovdmitry/czii-model-comparison.git
+cd DeePiCt
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda3
+rm miniconda.sh
+
+source $HOME/miniconda3/etc/profile.d/conda.sh
+
+echo 'source $HOME/miniconda3/bin/activate' >> ~/.bashrc
+
+conda install -n base -c conda-forge mamba -y
+
+mamba create -c conda-forge -c bioconda -n snakemake snakemake==5.13.0 python=3.7 -y
+conda activate snakemake
+
+conda install pandas -y
+
+conda install -c pytorch pytorch-gpu torchvision -y
+
+conda install -c conda-forge keras-gpu=2.3.1 -y
+
+echo "DeePiCt installation completed successfully!"
+echo "To use DeePiCt, activate the environment with: conda activate snakemake"
+echo "For 2D CNN pipeline: bash /path/to/2d_cnn/deploy_cluster.sh /path/to/config.yaml"
+echo "For 3D CNN pipeline: bash /path/to/3d_cnn/deploy_cluster.sh /path/to/config.yaml"
+echo "For local deployment use deploy_local.sh instead of deploy_cluster.sh"
+
+mkdir -p /home/ubuntu/czii-cryo-et-object-identification
+
+source $HOME/miniconda3/bin/activate
+pip install dotenv zarr emfile mrcfile
+
+
